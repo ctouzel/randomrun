5cdbe07eb9f6c3c2b8881c96a28a091c11adcfc9
ctouzel <ctouzel@gmail.com>
2014-12-07 15:59:24 -0500

    First

---
+++ RandomRun/Hello.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: Dec 7, 2014
+ *      Author: Christian Touzel
+ */
+
+#include "Hello.h"
+
+Hello::Hello() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Hello::~Hello() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ RandomRun/Hello.h
@@ -0,0 +1,17 @@
+/*
+ * Hello.h
+ *
+ *  Created on: Dec 7, 2014
+ *      Author: Christian Touzel
+ */
+
+#ifndef HELLO_H_
+#define HELLO_H_
+
+class Hello {
+public:
+	Hello();
+	virtual ~Hello();
+};
+
+#endif /* HELLO_H_ */
